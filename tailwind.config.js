6a97e10e0458b0bc5ca60e0dfe098c9b06515a09
LucioFider <fidelislucio404@gmail.com>
2023-03-03 17:24:28 +1000

    2nd-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,35 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      black: "#191A19",
+      yellow: "#FFF734",
+      green: "#68F590",
+      grey: "#F3f3f3",
+      "grey-light": "#fafafa",
+      "green-light": "#d2eff1",
+      white: "#fff",
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      lg: "1.15rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+      "6xl": "3.5rem",
+    },
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
   plugins: [],
 };
